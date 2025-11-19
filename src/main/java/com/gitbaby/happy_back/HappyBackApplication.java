8f425b60445442c7769f871e97603e92f0544f7d
manlubo <manlubo11@gmail.com>
2025-11-19 21:06:30 +0900

    start happy_project

---
+++ src/main/java/com/gitbaby/happy_back/HappyBackApplication.java
@@ -0,0 +1,13 @@
+package com.gitbaby.happy_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HappyBackApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(HappyBackApplication.class, args);
+  }
+
+}
