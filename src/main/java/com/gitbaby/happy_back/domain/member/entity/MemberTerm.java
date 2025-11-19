0dc071419acfec72ff757069b7b49903bb355427
manlubo <manlubo11@gmail.com>
2025-11-20 02:11:11 +0900

    엔티티 생성 - member_term

---
+++ src/main/java/com/gitbaby/happy_back/domain/member/entity/MemberTerm.java
@@ -0,0 +1,36 @@
+package com.gitbaby.happy_back.domain.member.entity;
+
+import com.gitbaby.happy_back.domain.common.entity.BaseCreatedEntity;
+import com.gitbaby.happy_back.domain.common.entity.BaseTimeEntity;
+import com.gitbaby.happy_back.domain.term.entity.Term;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "member_term")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@ToString(exclude = {"member"})
+public class MemberTerm extends BaseTimeEntity {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Column(name = "member_term_id")
+  private Long id;
+
+  @ManyToOne(fetch = FetchType.LAZY, optional = false)
+  @JoinColumn(name = "member_id", nullable = false)
+  private Member member;
+
+  @ManyToOne(fetch = FetchType.LAZY, optional = false)
+  @JoinColumn(name = "term_id", nullable = false)
+  private Term term;
+
+  @Column(nullable = false)
+  private Boolean agreed;
+
+  @Column(nullable = false)
+  private String version;
+}
