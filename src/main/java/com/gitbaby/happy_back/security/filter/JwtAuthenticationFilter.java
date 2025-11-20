f408cc32567b70d504e86c26570b7e87edbaf338
manlubo <manlubo11@gmail.com>
2025-11-20 20:04:28 +0900

    시큐리티 기본 구조 생성 - JwtFilter - MemberAuthDTO - SecurityConfig

---
+++ src/main/java/com/gitbaby/happy_back/security/filter/JwtAuthenticationFilter.java
@@ -0,0 +1,22 @@
+package com.gitbaby.happy_back.security.filter;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Log4j2
+@RequiredArgsConstructor
+@Component
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+  @Override
+  protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+
+  }
+}
