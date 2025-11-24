284ae8f879abcf6bbee54dd4bf031ab94ebc2de0
manlubo <manlubo11@gmail.com>
2025-11-24 23:41:40 +0900

    JwtUtil 수정 - 유효성 검증 Long타입으로 TTL 반환하도록 변경 - 주석 추가 - 필요한 것만 파라미터로 받음
    SecurityConfig Cors 설정 추가
    
    CookieUtil 추가
    - 개발, 운영환경 환경변수 분리

--- src/main/java/com/gitbaby/happy_back/security/config/SecurityConfig.java
+++ src/main/java/com/gitbaby/happy_back/security/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.gitbaby.happy_back.security.config;
 
 import com.gitbaby.happy_back.domain.member.en.Role;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -12,10 +13,17 @@ import org.springframework.security.config.annotation.web.configurers.AbstractHt
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.List;
 
 @Configuration
 @RequiredArgsConstructor
 public class SecurityConfig {
+  @Value("${custom.frontend-url}")
+  private String frontendUrl;
 
   @Bean
   public BCryptPasswordEncoder bCryptPasswordEncoder() {
@@ -43,5 +51,30 @@ public class SecurityConfig {
       .sessionManagement(sessionManagement -> sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
 
     return http.build();
+  }
+
+  @Bean
+  CorsConfigurationSource corsConfigurationSource() {
+    // CORS 정책 객체 생성
+    CorsConfiguration configuration = new CorsConfiguration();
+
+    // 주소 설정
+    configuration.setAllowedOrigins(List.of(frontendUrl));
+    configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"));
+
+    // 쿠키, Authorization 같은 자격 증명 정보를 포함한 요청 허용
+    configuration.setAllowCredentials(true);
+
+    // 브라우저가 요청할 때 추가하는 모든 커스텀 헤더를 허용 ("Authorization", "Content-Type" 이런식으로 사용하기도 함)
+    configuration.setAllowedHeaders(List.of("*"));
+
+    // CORS 매핑 소스 생성 (URL 패턴별로 CORS 정책을 등록하는 역할)
+    UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+
+    // 모든 경로("/**")에 대해 위에서 정의한 configuration 적용
+    source.registerCorsConfiguration("/**", configuration);
+
+    // Spring Security filter chain에서 참조할 수 있도록 Bean 반환
+    return source;
   }
 }
---
+++ src/main/java/com/gitbaby/happy_back/security/util/CookieUtil.java
@@ -0,0 +1,111 @@
+package com.gitbaby.happy_back.security.util;
+
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseCookie;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+public class CookieUtil {
+  @Value("${jwt.refresh-expiration-days}")
+  private long expireDays;
+
+  @Value("${jwt.expiration-minutes}")
+  private long expirationMinutes;
+
+  @Value("${cookie.secure}")
+  private boolean secure;
+
+  @Value("${cookie.domain}")
+  private String domain;
+
+  @Value("${cookie.same-site}")
+  private String sameSite;
+
+  // 쿠키 공통 로직
+  private ResponseCookie.ResponseCookieBuilder baseCookie(String name, String value) {
+
+    ResponseCookie.ResponseCookieBuilder builder = ResponseCookie
+      .from(name, value)
+      .httpOnly(true)
+      .secure(secure)
+      .sameSite(sameSite)
+      .path("/");
+
+    if (domain != null && !domain.isBlank()) {
+      builder.domain(domain);
+    }
+
+    return builder;
+  }
+
+  // 엑세스 쿠키 생성
+  public ResponseCookie createAccessCookie(String accessToken) {
+    return baseCookie("HP_ACCESS", accessToken)
+      .maxAge(expirationMinutes * 60)
+      .build();
+  }
+
+  // 엑세스 쿠키 삭제
+  public ResponseCookie deleteAccessCookie() {
+    return baseCookie("HP_ACCESS", "")
+      .maxAge(0)
+      .build();
+  }
+
+
+  // 리프레쉬 쿠키 생성
+  public ResponseCookie createRefreshCookie(String refreshToken, boolean rememberMe) {
+    long maxAge = rememberMe ? expireDays * 24 * 60 * 60 : -1;
+
+    return baseCookie("HP_REFRESH", refreshToken)
+      .maxAge(maxAge)
+      .build();
+  }
+
+  // 리프레쉬 쿠키 삭제
+  public ResponseCookie deleteRefreshCookie() {
+    return baseCookie("HP_REFRESH", "")
+      .maxAge(0)
+      .build();
+  }
+
+  // 로그인시 사용(쿠키 전체 발급)
+  public List<ResponseCookie> createLoginCookies(String accessToken, String refreshToken, boolean rememberMe) {
+    return List.of(
+      createAccessCookie(accessToken),
+      createRefreshCookie(refreshToken, rememberMe)
+    );
+  }
+
+  // 로그아웃시 사용(쿠키 전체 삭제)
+  public List<ResponseCookie> createLogoutCookies() {
+    return List.of(
+      deleteAccessCookie(),
+      deleteRefreshCookie()
+    );
+  }
+
+  // 쿠키 존재 여부 확인
+  public boolean hasCookie(HttpServletRequest request, String cookieName) {
+    return getCookieValue(request, cookieName) != null;
+  }
+
+  // 쿠기 값 읽기
+  public String getCookieValue(HttpServletRequest request, String cookieName) {
+    if (request.getCookies() == null) {
+      return null;
+    }
+
+    for (Cookie cookie : request.getCookies()) {
+      if (cookie.getName().equals(cookieName)) {
+        return cookie.getValue();
+      }
+    }
+
+    return null;
+  }
+}
