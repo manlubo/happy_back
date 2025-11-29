b6fffa1bc8dbe924844b99856701666840bccd00
manlubo <manlubo11@gmail.com>
2025-11-29 14:04:11 +0900

    CategoryRepository 생성

---
+++ src/main/java/com/gitbaby/happy_back/domain/category/repository/CategoryRepository.java
@@ -0,0 +1,9 @@
+package com.gitbaby.happy_back.domain.category.repository;
+
+import java.util.Locale.Category;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+
+}
