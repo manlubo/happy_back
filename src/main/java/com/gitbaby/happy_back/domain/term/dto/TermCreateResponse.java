b008cfdd750bd255e3c00272a1e37e2036ab6811
manlubo <manlubo11@gmail.com>
2025-11-29 17:34:51 +0900

    VersionUtil 생성(버전 관리용), TermService 생성(약관 등록)

---
+++ src/main/java/com/gitbaby/happy_back/domain/term/dto/TermCreateResponse.java
@@ -0,0 +1,21 @@
+package com.gitbaby.happy_back.domain.term.dto;
+
+import com.gitbaby.happy_back.domain.term.en.TermType;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class TermCreateRequest {
+  @NotNull(message = "약관 타입은 필수 입력항목입니다.")
+  private TermType type;
+
+  @NotBlank(message = "약관 내용은 필수 입력항목입니다.")
+  private String content;
+
+  @NotNull(message = "필수여부는 필수 입력항목입니다.")
+  private boolean required;
+}
