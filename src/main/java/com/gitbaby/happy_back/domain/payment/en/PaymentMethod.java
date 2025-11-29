363e2a7eb2abb25f90c9c1e1e4d192956c050946
manlubo <manlubo11@gmail.com>
2025-11-29 13:34:52 +0900

    Payment, PaymentHistory 엔티티 생성

---
+++ src/main/java/com/gitbaby/happy_back/domain/payment/en/PaymentMethod.java
@@ -0,0 +1,5 @@
+package com.gitbaby.happy_back.domain.payment.en;
+
+public enum PaymentMethod {
+  CARD, TOSS, PAYCO, KAKAO_PAY, NAVER_PAY
+}
---
+++ src/main/java/com/gitbaby/happy_back/domain/payment/en/PaymentStatus.java
@@ -0,0 +1,5 @@
+package com.gitbaby.happy_back.domain.payment.en;
+
+public enum PaymentStatus {
+  PENDING, COMPLETED, FAILED, REFUNDED
+}
